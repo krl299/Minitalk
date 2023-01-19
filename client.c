ac265e66f4ed693ca4a39ed3e5cbce99089414d8
PeliAzuh <47522008+krl299@users.noreply.github.com>
2023-01-19 17:28:58 +0100

    Add files via upload
    mandatory part done

--- client.c
+++ client.c
@@ -6,14 +6,61 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:09:28 by cmoran-l          #+#    #+#             */
-/*   Updated: 2022/11/28 12:39:18 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/01/19 17:01:36 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+/* Receive server pid and msg to send back to server process */
+int	main(int argc, char **argv)
 {
-	ft_printf("%","");
-	return(0);
+	int		server_pid;
+	char	*msg;
+
+	signal(SIGUSR2, ft_handler2);
+	if (argc != 3)
+	{
+		ft_printf("./client [server-pid] [message]\n");
+		exit(0);
+	}
+	server_pid = ft_atoi(argv[1]);
+	msg = argv[2];
+	ft_send_msg(msg, server_pid);
+	return (0);
+}
+
+/* Server dispatched message */
+void	ft_handler2(int sig)
+{
+	ft_printf("Message dispatched\n");
+}
+
+/* Send the message to server process */
+void	ft_send_msg(char *msg, int pid)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (msg[++i])
+		ft_send_byte((unsigned)msg[i], pid);
+	ft_send_byte('\0', pid);
+}
+
+/* Send byte to pid */
+void	ft_send_byte(unsigned char c, int pid)
+{
+	int	j;
+
+	j = 7;
+	while (j >= 0)
+	{
+		if (c >> j & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(1);
+		j--;
+	}
 }
--- minitalk.h
+++ minitalk.h
@@ -5,10 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/28 09:53:00 by cmoran-l          #+#    #+#             */
-/*   Updated: 2022/11/28 09:56:16 by cmoran-l         ###   ########.fr       */
+/*   Created: 2023/01/11 12:26:50 by cmoran-l          #+#    #+#             */
+/*   Updated: 2023/01/19 17:03:04 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-
+#ifndef MINITALK_H
+# define MINITALK_H
+# include	"libft/libft.h"
+# include	<signal.h>
+/* Funtions */
+void	ft_send_msg(char *str, int pid);
+void	ft_send_byte(unsigned char c, int pid);
+void	ft_handler(int pid, siginfo_t *si, void *unused);
+void	ft_handler2(int pid);
+#endif
